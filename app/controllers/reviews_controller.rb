d205ffb5be4286244ce7d9569eab6b599771f4d0
Laila Atrmouh <laila.atrmouh@gmail.com>
2012-10-09 18:59:48 -0400

    Removing reviews from the website

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,19 @@
 class ReviewsController < ApplicationController
+
+  # Create a review
   def create
     @book = Book.find(params[:book_id])
     @review = @book.reviews.create(params[:review])
     redirect_to book_path(@book)
   end
+  
+  # Remove a review
+  def destroy
+    @book = Book.find(params[:book_id])
+    @review = @book.reviews.find(params[:id])
+    @review.destroy
+    redirect_to book_path(@book)
+  end
+ 
+ 
 end
