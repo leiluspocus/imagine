be691af2e6ef94d23213bd6898ff277fe28a35a9
Laila Atrmouh <laila.atrmouh@gmail.com>
2012-10-28 22:11:32 -0400

    R reviews from the website" Revert
    This reverts commit d205ffb5be4286244ce7d9569eab6b599771f4d0.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,19 +1,7 @@
 class ReviewsController < ApplicationController
-
-  # Create a review
   def create
     @book = Book.find(params[:book_id])
     @review = @book.reviews.create(params[:review])
     redirect_to book_path(@book)
   end
-  
-  # Remove a review
-  def destroy
-    @book = Book.find(params[:book_id])
-    @review = @book.reviews.find(params[:id])
-    @review.destroy
-    redirect_to book_path(@book)
-  end
- 
- 
 end
