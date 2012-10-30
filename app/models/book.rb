093d1e428f18a68bc5b720608b554a0e9eda34e3
Laila Atrmouh <laila.atrmouh@gmail.com>
2012-10-30 19:19:26 -0400

    Book Rating implementation

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 class Book < ActiveRecord::Base
   attr_accessible :author, :resume, :title
-  
+  attr_accessor :rating
   validates :author,  :presence => true
   validates :resume, :presence => true, :length => { :minimum => 50 }
   validates :title, :presence => true,
@@ -9,22 +9,21 @@ class Book < ActiveRecord::Base
   has_many :reviews
   
   def show
-  @book = Book.find(params[:id])
+  bookid = params[:id]
+  @book = Book.find(bookid)
  
+  rtg = 0
+  # Fetching all the reviews associated to the book
+  @reviews = Review.find(:conditions => ["book_id = ?", bookid])   
+  # Computing the rating
+  @reviews.each do |review|
+  	rtg += review.grade
+  end
+  @rating = rtg / @reviews.size 
   	respond_to do |format|
     	format.html  # book.html.erb
     	format.json  { render :json => @post }
   	end
-  end
-  
-  def computeRating
-#  rating = 0;
-#  reviews = Review.where(:book=>self);
-#  	reviews.each do |rev|
-#  		rating += rev.grade;
-#  	end
-#  return rating/reviews.length
-	return 5
   end
   
 end
