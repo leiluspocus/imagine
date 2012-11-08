964ccbbd781dd01f26e41efdf0139d7bfa0b6e47
Laila Atrmouh <laila.atrmouh@gmail.com>
2012-11-08 15:44:28 -0500

    fixed: a sneaky "w" was breaking my tests...

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ActiveRecord::Base
-  attr_accessible :author, :resume, :titlew 
+  attr_accessible :author, :resume, :title 
   
   validates :author,  :presence => true
   validates :resume, :presence => true, :length => { :minimum => 50 }
